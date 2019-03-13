719f288f633e81cd1da801082ef717f8a8a4195b
Zach Tom <zachptom@gmail.com>
2019-03-13 02:09:09 -0700

    Changed to ODFDS

--- src/components/test.js
+++ src/components/test.js
@@ -1,10 +1,18 @@
 import React from 'react';
+//import { relative } from 'path';
+
+//ignore this I'm still learnign js + react lol- Zach
+const styles = {
+  center: {
+    alignSelf: 'center'
+  }
+}
 
 const HelloTest = (props) => {
   console.log(props.data)
   return (
-    <div>
-      <h1> Successful Login! </h1>
+    <div className={styles.center}>
+      <h1> On Demand Food Delivery Service </h1>
     </div>
   );
 }
