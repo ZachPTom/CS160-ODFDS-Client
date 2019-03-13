5de0ad2476312323a213440302f4330f3d0899c8
Zach Tom <zachptom@gmail.com>
2019-03-13 01:26:03 -0700

    Changed hello message to "successful login!"

--- src/components/test.js
+++ src/components/test.js
@@ -4,7 +4,8 @@ const HelloTest = (props) => {
   console.log(props.data)
   return (
     <div>
-      Hello!
+      <h1 style="text-align:center;font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;"><span 
+    >Successful Login!</span></h1>
     </div>
   );
 }
