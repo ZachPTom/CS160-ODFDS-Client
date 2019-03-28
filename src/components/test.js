219582f9c3aea30e7474cdc7c7b4e32d65e0e2f9
jcsandoval929 <jcsandoval929@gmail.com>
2019-03-27 17:49:08 -0700

    JC UI changes

--- src/components/test.js
+++ src/components/test.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CardActions from '@material-ui/core/CardActions';
-import { Link, Router, Route } from 'react-router-dom';
-import OrderHistory from "./orderHistory";
-import AddressForm from "./order_info";
-import Map from "./map";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+import { Link } from "react-router-dom";
+import "typeface-roboto";
 
 const HelloTest = () => {
   return (
-  <React.Fragment>
-        <h1 align="center">Welcome Restaurant</h1>
+    <React.Fragment>
+      <h1 align="center" style={{ fontFamily: "roboto" }}>
+        Welcome Driver
+      </h1>
 
-  <hr size="100px"/>
-  <CardActions style={{justifyContent: 'center'}}>
-    <Button component={Link} to="/orderHistory">
-      Order History
-    </Button>
-    <Button component={Link} to="/place_order">
-      Place Order
-    </Button>
-    <Button component={Link} to="/map">
-      Restaurant Location
-    </Button>
-    </CardActions>
-  </React.Fragment>
+      <hr size="100px" />
+      <CardActions style={{ justifyContent: "center" }}>
+        <Button
+          variant="outlined"
+          color="secondary"
+          component={Link}
+          to="/orderHistory"
+        >
+          Order History
+        </Button>
+        <Button
+          variant="outlined"
+          color="secondary"
+          component={Link}
+          to="/onShift"
+        >
+          Start Shift
+        </Button>
+        <Button variant="outlined" color="secondary" component={Link} to="/map">
+          Restaurant Location
+        </Button>
+      </CardActions>
+    </React.Fragment>
   );
-}
+};
 
 export default HelloTest;
